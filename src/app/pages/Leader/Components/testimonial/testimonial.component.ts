8a207244a784ab174ae054b89dc3c2e8fe026b25
MoustafaAhmed12 <ma0558317@gmail.com>
2024-08-17 01:57:31 +0200

    complete dashboard in leader

---
+++ src/app/pages/Leader/Components/testimonial/testimonial.component.ts
@@ -0,0 +1,59 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { DashboardService } from '../../services/dashboard.service';
+import { dashboardFeedbacks } from '../../model/dashboard';
+import { NgClass } from '@angular/common';
+
+@Component({
+  selector: 'app-testimonial',
+  standalone: true,
+  imports: [NgClass],
+  templateUrl: './testimonial.component.html',
+  styleUrl: './testimonial.component.scss',
+})
+export class TestimonialComponent implements OnInit {
+  dashboardService = inject(DashboardService);
+  currentPage: number = 0;
+  dashboardFeedbacks: dashboardFeedbacks[] = [];
+  ngOnInit() {
+    this.fetchDashboardFeedbacks();
+    console.log(this.getStarsArray(4));
+  }
+
+  fetchDashboardFeedbacks(): void {
+    this.dashboardService.dashboardFeedbacks().subscribe({
+      next: ({ statusCode, data }) => {
+        if (statusCode === 200) {
+          this.dashboardFeedbacks = data;
+        } else {
+        }
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+  prevTestimonial(): void {
+    if (this.currentPage > 0) {
+      this.currentPage--;
+    }
+  }
+
+  nextTestimonial(): void {
+    if (this.currentPage < this.dashboardFeedbacks.length - 1) {
+      this.currentPage++;
+    }
+  }
+
+  getStarsArray(rating: number): string[] {
+    const fullStars = Math.floor(rating);
+    const halfStar = rating % 1 >= 0.5;
+    const emptyStars = 5 - fullStars - (halfStar ? 1 : 0);
+
+    return [
+      ...Array(fullStars).fill('filled'),
+      ...Array(halfStar ? 1 : 0).fill('half'),
+      ...Array(emptyStars).fill('empty'),
+    ];
+  }
+}
