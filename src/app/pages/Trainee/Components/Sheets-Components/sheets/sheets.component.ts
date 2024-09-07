f7e2b33bcec9b034bfbf7ec759ef7eafd75665f3
hassan alaa <hassanalaa6060@gmail.com>
2024-09-07 03:48:35 +0300

    fix UI

--- src/app/pages/Trainee/Components/Sheets-Components/sheets/sheets.component.ts
+++ src/app/pages/Trainee/Components/Sheets-Components/sheets/sheets.component.ts
@@ -44,7 +44,7 @@ export class SheetsComponent implements OnInit {
   // Fetches material data for a specific sheet by ID and sets the material name
   updateMatrial(id: any, matrialName: string): void {
     this.loadingSheet=true
-    this.addActiveCard(id)
+    this.activeCard(id)
     this.matrialName = matrialName; // Set the name of the selected material
     this._sheetService.getMaterialsInSheet(id).subscribe({
       next: ({ statusCode, data }) => {
@@ -61,11 +61,13 @@ export class SheetsComponent implements OnInit {
   public openLinkMatrial(url: string): void {
     window.open(url, '_blank'); // Open the URL in a new browser tab
   }
-  addActiveCard(id:any):void{
-    $(`.card-bg-img`).removeClass('border-card');
-    $(`button`).removeClass('border-btn');
-    $(`#${id} .card-bg-img`).addClass('border-card');
-    $(`#${id} button`).addClass('border-btn');
+  activeCard(id:any):void{
+    $(`.content`).removeClass('border-card');
+    $(`.btn`).removeClass('border-btn');
+    $(`#${id} .content`).addClass('border-card');
+    $(`#${id}.btn`).addClass('border-btn');
+    console.log(    $(`#${id}button`)  );
+
   }
   openLink(url:string):void{
     window.open(url,'_blank')
