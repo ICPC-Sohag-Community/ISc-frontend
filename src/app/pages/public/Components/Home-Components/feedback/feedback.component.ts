838a92f04a36fed700b9d4ed961f96ccf9bbbf4f
hassan alaa <hassanalaa6060@gmail.com>
2024-09-15 21:37:20 +0300

    guest page

---
+++ src/app/pages/public/Components/Home-Components/feedback/feedback.component.ts
@@ -0,0 +1,49 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { SwiperModule } from 'swiper/angular';
+import SwiperCore, { SwiperOptions } from 'swiper';
+import Swiper, { Navigation } from 'swiper';
+
+@Component({
+  selector: 'app-feedback',
+  standalone: true,
+  imports: [CommonModule,SwiperModule],
+  templateUrl: './feedback.component.html',
+  styleUrls: ['./feedback.component.scss']
+})
+export class FeedbackComponent {
+
+  stars:number[]=[1,2,3,4,5]
+
+  // Reference to Swiper instance
+  private swiper?: SwiperCore;
+
+    // Swiper configuration options
+    config1: SwiperOptions = {
+      slidesPerView: 3, // Number of slides visible at a time
+      spaceBetween: 80, // Space between slides
+      navigation: false, // Disable built-in navigation
+      pagination: { clickable: false }, // Disable clickable pagination
+      scrollbar: false, // Disable scrollbar
+      autoplay: false, // Disable autoplay
+    };
+
+    // Method to handle Swiper instance when it's ready
+    onSwiper(swiper: SwiperCore) {
+      this.swiper = swiper;
+    }
+
+    // Method to move to the next slide
+    nextSlide() {
+      if (this.swiper) {
+        this.swiper.slideNext();
+      }
+    }
+
+    // Method to move to the previous slide
+    prevSlide() {
+      if (this.swiper) {
+        this.swiper.slidePrev();
+      }
+    }
+}
--- src/app/pages/public/screens/home-public/home-public.component.ts
+++ src/app/pages/public/screens/home-public/home-public.component.ts
@@ -3,9 +3,9 @@ import { HomeHeroComponent } from '../../Components/Home-Components/home-hero/ho
 import { AboutUsComponent } from "../../Components/Home-Components/about-us/about-us.component";
 import { AchievementsComponent } from "../../Components/Home-Components/achievements/achievements.component";
 import { CommonModule } from '@angular/common';
-import { SwiperModule } from 'swiper/angular';
-import SwiperCore, { SwiperOptions } from 'swiper';
-import Swiper, { Navigation } from 'swiper';
+import { FoundersComponent } from "../../Components/Home-Components/founders/founders.component";
+import { CoachesComponent } from "../../Components/Home-Components/coaches/coaches.component";
+import { FeedbackComponent } from "../../Components/Home-Components/feedback/feedback.component";
 
 declare var $: any;
 
@@ -17,80 +17,14 @@ declare var $: any;
     AboutUsComponent,
     AchievementsComponent,
     CommonModule,
-    SwiperModule
+    FoundersComponent,
+    CoachesComponent,
+    FeedbackComponent
 ],
   templateUrl: './home-public.component.html',
   styleUrls: ['./home-public.component.scss']
 })
 export class HomePublicComponent  {
-
-  achievements:any[]=[
-    {
-      id:1,
-      img:"../../../../../assets/img_public/achivements_1.png",
-      text:"4th on Egypt (ECPC 2024)"
-    },
-    {
-      id:2,
-      img:"../../../../../assets/img_public/achivements_2.png",
-      text:"10th on Egypt (ECPC 2023)"
-    },
-    {
-      id:3,
-      img:"../../../../../assets/img_public/achivements_3.png",
-      text:"Helped 147 students to participate in ECPC Qualifications (2022)"
-    }
-  ]
-  founders:any[]=[
-    {
-      id:1,
-      img:"../../../../../assets/img_public/zyad.png",
-      name:"Zyad Bahaa"
-    },
-    {
-      id:2,
-      img:"../../../../../assets/img_public/Ahmed.png",
-      name:"Ahmed Alaa"
-    },
-    {
-      id:3,
-      img:"../../../../../assets/img_public/karim.png",
-      name:"Karim Abdelrhman"
-    }
-  ]
-  stars:number[]=[1,2,3,4,5]
-
-  // Reference to Swiper instance
-  private swiper?: SwiperCore;
-
-    // Swiper configuration options
-    config1: SwiperOptions = {
-      slidesPerView: 3, // Number of slides visible at a time
-      spaceBetween: 80, // Space between slides
-      navigation: false, // Disable built-in navigation
-      pagination: { clickable: false }, // Disable clickable pagination
-      scrollbar: false, // Disable scrollbar
-      autoplay: false, // Disable autoplay
-    };
-
-    // Method to handle Swiper instance when it's ready
-    onSwiper(swiper: SwiperCore) {
-      this.swiper = swiper;
-    }
-
-    // Method to move to the next slide
-    nextSlide() {
-      if (this.swiper) {
-        this.swiper.slideNext();
-      }
-    }
-
-    // Method to move to the previous slide
-    prevSlide() {
-      if (this.swiper) {
-        this.swiper.slidePrev();
-      }
-    }
 
 }
 
