b0ab1791bb93ed9d09389b27145e6b56d8bee30b
hassan alaa <hassanalaa6060@gmail.com>
2024-09-16 04:26:21 +0300

    fix scrolling

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { routes } from './app/app.routes';
-import { provideRouter, RouterModule } from '@angular/router';
-import { importProvidersFrom } from '@angular/core';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(routes),
-    importProvidersFrom(
-      RouterModule.forRoot(routes, {
-        scrollPositionRestoration: 'enabled', // Restore scroll position on back/forward
-        anchorScrolling: 'enabled', // Enable anchor scrolling
-      })
-    ),
-  ],
-}).catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
